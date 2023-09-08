ce31a234e78c2d3eaaa549baa9cdf47f546b48d7
b4rt <root.b4rt@gmail.com>
2023-09-07 19:02:00 -0500

    Adding Meta-Tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head';
 import { Grid, Paper } from '@mantine/core';
 import React from 'react';
-import AppHeader from '../components/AppHeader/AppHeader.';
+import AppHeader from '../components/AppHeader/AppHeader';
 import { links } from '../config/AppMenu';
-import ConsoleEmulator from '../components/ConsoleEmulator/ConsoleEmulator.';
+import ConsoleEmulator from '../components/ConsoleEmulator/ConsoleEmulator';
 import AppBody from '../components/AppBody/AppBody.';
-import { AppFooter } from '../components/AppFooter/AppFooter.';
+import { AppFooter } from '../components/AppFooter/AppFooter';
 
 export default function HomePage() {
     return (
@@ -14,6 +14,12 @@ export default function HomePage() {
                 <title>b4rt.dev</title>
                 <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
                 <link rel="shortcut icon" href="/favicon.png" />
+                <meta name="title" content="B4rt.dev - Jose Manuel's Software Engineering Portfolio" />
+                <meta
+                  name="description"
+                  content="Software Engineer, Python Developer, Linux Enthusiast, Security Researcher, and more.."
+                />
+
             </Head>
 
             <AppHeader links={links} />
